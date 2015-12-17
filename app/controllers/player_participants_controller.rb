f3d5d5d450b4e6a573c7cbba92aeaa3350bb5e90
Matt Fair <matt.j.fair@gmail.com>
2015-12-16 23:36:02 -0500

    players added after tournament assigned now follow tournament.  Added all announcments to home page.  Need to make them look better

--- app/controllers/player_participants_controller.rb
+++ app/controllers/player_participants_controller.rb
@@ -3,6 +3,10 @@ class PlayerParticipantsController < ApplicationController
   def create
     @player = PlayerParticipant.new(player_participant_params)
     if @player.save
+      player_follows_team(@player.team_id, @player.player_id)
+      if @player.team.tournament
+        player_follows_tournament(@player.team.tournament_id, @player.player_id)
+      end
       redirect_to team_path(@player.team)
     else
       flash[:notice] = @player.errors
@@ -20,4 +24,17 @@ class PlayerParticipantsController < ApplicationController
   def player_participant_params
     params.require(:player_participant).permit(:team_id, :player_id)
   end
+
+  def player_follows_team(team_id, player_id)
+    team = Team.find(team_id)
+    user = User.find(Player.find(player_id).user_id)
+    user.follow(team)
+  end
+
+  def player_follows_tournament(tournament_id, player_id)
+    tournament = Tournament.find(tournament_id)
+    user = User.find(Player.find(player_id).user_id)
+    user.follow(tournament)
+  end
+
 end
