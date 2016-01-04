d1a8f2c57e20332dc0a05a54712e5451d81e68f1
Matt Fair <matt.j.fair@gmail.com>
2016-01-03 22:11:51 -0500

    fix new player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
 
-  validates_presence_of :date_of_birth, :jersey_number, :user_id, :first_name, :last_name
+  validates_presence_of :date_of_birth, :user_id, :first_name, :last_name
 
   attachment :player_photo, content_type: ["image/jpeg", "image/png", "image/gif"]
   belongs_to :user
