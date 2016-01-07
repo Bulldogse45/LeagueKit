f5276ef2a8e92967873dadf5c70745f2f2a05ed3
Matt Fair <matt.j.fair@gmail.com>
2016-01-07 18:57:32 -0500

    Only league owners can remove teams from league.  This closes #118.

--- app/controllers/league_ties_controller.rb
+++ app/controllers/league_ties_controller.rb
@@ -1,11 +1,20 @@
 class LeagueTiesController < ApplicationController
-
+  before_action :require_user, :check_user_is_owner_or_coach
 
   def destroy
     @league_ty = LeagueTy.find(params[:id])
     @league_ty.destroy
     flash[:success] = "Team was removed"
     redirect_to :back
+  end
+
+  private
+
+  def check_user_is_owner_or_coach
+    unless current_user ==  LeagueTy.find(params[:id]).league.user
+      flash[:alert] = "You need to be the tournament director to remove a team"
+      redirect_to :back
+    end
   end
 
 end
