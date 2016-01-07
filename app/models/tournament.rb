48d68827d68043f72f2ac22181ea179607bea6f8
Matt Fair <matt.j.fair@gmail.com>
2016-01-06 20:39:16 -0500

    Tournaments cannot be created in the past.  This closes #96.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,7 +2,7 @@ class Tournament < ActiveRecord::Base
   has_many :announces, as: :announcable
   acts_as_followable
   validates_presence_of :name, :start_time, :end_time, :league_id
-  before_save :end_after_begin
+  before_save :end_after_begin, :begins_in_future
 
   has_many :teams
   has_many :games
@@ -59,6 +59,15 @@ class Tournament < ActiveRecord::Base
   def end_after_begin
     if self.end_time < self.start_time
       errors.add(:end_time, "A tournament cannot end before it begins.")
+      false
+    else
+      true
+    end
+  end
+
+  def begins_in_future
+    if Time.now - 12.hours > self.start_time
+      errors.add(:start_time, "Currently tournaments must be created within 12 hours of the start time.")
       false
     else
       true
